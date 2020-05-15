79121ff496f3a832413ebf9fee31c636c34b2eae
Leandro Arns <leandro.arns@gmail.com>
2020-05-15 18:04:02 -0300

    styles do componente Toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import SignIn from './pages/Signin/index';
 // import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
